0f3e8c09dde89495b2476584f199b4b91d8643b9
Dragos Dascalita Haut <ddragosd@gmail.com>
2016-03-18 18:13:18 -0700

    updates for executing a test downloaded from a URI on the tsung-master

--- scripts/tsung-runner.sh
+++ scripts/tsung-runner.sh
@@ -14,6 +14,7 @@ service sshd start
 
 # start crontab to update tsung hosts found in the cluster
 echo ${MARATHON_URL} > /etc/tsung/marathon_url
+tsung-update-hosts
 crontab /etc/crontab
 service crond start
 
@@ -28,10 +29,13 @@ fi
 # http://stackoverflow.com/questions/2369341/which-tcp-port-does-erlang-use-for-connecting-to-a-remote-node
 # erl -kernel inet_dist_listen_min 9001 inet_dist_listen_max 9005
 
+echo "sleeping for 1min to allow tsung-slaves to find the tsung-master node ..."
+sleep 1m
+
 current_date=$(date +%Y%m%d-%H%M)
 echo "Tsung log directory should be ${current_date}"
 cmd='tsung -l /usr/local/tsung/ '$@
 echo "Executin ${cmd} ..."
 ${cmd}
-cd /usr/local/tsung/${current_date}/ && /usr/lib64/tsung/bin/tsung_stats.pl
+cd /usr/local/tsung/${current_date}/ && /usr/lib/tsung/bin/tsung_stats.pl
 
